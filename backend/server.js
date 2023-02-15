41bd08495554c3d78c86085ee0a3825065d53624
Sherzod <shermanjones99@gmail.com>
2023-02-15 17:02:09 -0500

    fix: added optional port value for api

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
-const PORT = 8000;
+const PORT = 8000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
